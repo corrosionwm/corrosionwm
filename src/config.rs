5c810d3f584b67d11519fea601af8ca8e1f89eac
Ash <ash@midiidev.xyz>
2023-04-11 12:53:14 -0400

    handle setting config dir in a better way (#26)
    * handle setting config dir in a better way
    
    * try everything before /home/username
    
    * HOME should exist, and remove references to username
    
    ---------
    
    Co-authored-by: electron271 <aveanzaldua@gmail.com>

--- src/config.rs
+++ src/config.rs
@@ -29,27 +29,15 @@ pub struct Defaults {
     pub launcher: String,
 }
 
-fn get_username() -> String {
-    let user;
-    match env::var("USER") {
-        Ok(val) => {
-            user = val;
-        }
-        Err(_) => {
-            let whoami_output = Command::new("whoami").output().unwrap();
-            user = String::from_utf8_lossy(&whoami_output.stdout).to_string();
-        }
-    }
-    user
-}
-
 impl CorrosionConfig {
     //initialize corrosion config
     pub fn new() -> Self {
-        //check if ~/.config/corrosionwm/config.toml exists
-        //if not, create it with the default configuration
-        let username = get_username();
-        let config_directory = format!("/home/{}/.config/corrosionwm", username);
+        // use $XDG_CONFIG_HOME, or fallback to $HOME/.config
+        let config_directory = match env::var("XDG_CONFIG_HOME") {
+            Ok(val) => format!("{}/corrosionwm", val),
+            Err(_) => format!("{}/.config/corrosionwm", env::var("HOME").unwrap())
+        };
+
         let config_file = format!("{}/config.toml", config_directory);
 
         //check for ~/.config/corrosionwm
