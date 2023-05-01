41c508559604b6a35293b2a7d811638c25c973e2
electron271 <aveanzaldua@gmail.com>
2023-05-01 12:16:45 -0500

    oops

--- build.rs
+++ build.rs
@@ -6,6 +6,6 @@ fn main() {
 
     // if the os is windows, say "why the fuck are you using windows"
     if cfg!(windows) {
-        panic!("Segmentation fault (core dumped)")
+        panic!("Segmentation fault (core dumped)");
     }
 }
